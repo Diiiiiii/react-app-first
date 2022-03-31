f616b51f657096f5aee8b4019e18b2dbe58e8661
Dijana Đuran <dijana1208@yahoo.com>
2022-03-31 12:46:20 +0200

    Add css file

--- src/componenets/RepositoryLink.jsx
+++ src/componenets/RepositoryLink.jsx
@@ -1,7 +1,9 @@
+import "./RepositoryLink.css";
+
 export function RepositoryLink() {
     return (
       <a
-            className="App-link"
+            className="RepositoryLink"
             href="https://github.com/Diiiiiii/react-app-first">
             GitHub Repository Link
           </a>
