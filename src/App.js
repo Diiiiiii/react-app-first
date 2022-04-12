8c60f1fa96de06c22f637e76f3fa60bd8c121082
Dijana Đuran <dijana1208@yahoo.com>
2022-04-12 15:23:19 +0200

    Add LoginForm componenet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { idHelpers } from "./library/helpers";
 //import { RandomValue } from "./components/RandomValue";
 import { RepositoryLink } from "./components/RepositoryLink";
 import { Quiz } from "./components/Quiz";
-import { useState} from "react"
+import { useState} from "react";
+import {LoginForm} from "./components/LoginForm";
 
 function App() {
   const [actionCount, setActionCount] = useState(0);
@@ -14,6 +15,9 @@ function App() {
   setQuizAnswer({answers, id});
 };
   const handleStateChange = () => setActionCount((state) => state + 1);
+  const handleLogin = (formState) => console.log(formState);
+  
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +27,7 @@ function App() {
         <p>Your action count is: {actionCount}</p>
         <RepositoryLink>View Repository</RepositoryLink>
         {/* <RandomValue values={[4, 5, 6]} /> */}
+        <LoginForm onLogin={handleLogin}/>
         {quizAnswer === null && (
           <Quiz id = {id} onSubmit={handleSubmit} onStateChange= {handleStateChange}/>
         )}
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function LoginForm(props) {
+    const [state, setState] = useState({});
+
+    const handleChange = (event) => {
+        console.log(event.target.value, event.target.name);
+       setState({...state, [event.target.name]: event.target.value});
+        //state[event.target.name] = event.target.value
+        //state.name = event.target.value
+    };
+
+    const handleSubmint = (event) => {
+        event.preventDefault();
+        console.log("submit", state);
+        props.onLogin(state);
+    };
+
+    return (
+     <form onSubmit={handleSubmint}>
+         <div>
+             <label htmlFor="name">
+                Name:
+                <input type="text" id="name" name="name" onChange={handleChange} />
+             </label>
+         </div>
+         <div>
+             <label htmlFor="">
+                 Email:
+                 <input type="email" name="email" id="email" onChange={handleChange} />
+             </label>
+         </div>
+         <div>
+             <button type="submit">Sign in</button>
+         </div>
+     </form>
+    );
+}
