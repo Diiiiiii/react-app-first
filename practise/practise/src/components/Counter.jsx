0bc2a22ba2039724214815bd97c21115c5451972
Dijana Duran <dijanaduran@gmail.com>
2022-04-13 15:50:14 +0200

    update Practise

--- practise/practise/src/components/Counter.jsx
+++ practise/practise/src/components/Counter.jsx
@@ -1,4 +1,11 @@
+import { useState} from "react";
+
 export function Counter() {
-    return <button>0</button>;
-    
+    const [count, setCount] = useState(1234);
+    const handleClick = () => {
+        //setCount(count + 1);
+        setCount((state) => state + 1);
+    };
+
+    return <button onClick= {handleClick}>{count}</button>;  
 }
